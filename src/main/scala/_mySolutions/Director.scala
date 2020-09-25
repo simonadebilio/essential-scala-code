28fe27d6e07f2e35d67f23d375698273ff312245
Simona Debilio <simona.debilio@gmail.com>
2020-09-25 16:58:29 +0100

    Add new exercise 3.1.6.3 (Directorial Debut)

--- src/main/scala/_mySolutions/Director.scala
+++ src/main/scala/_mySolutions/Director.scala
@@ -1,40 +1,62 @@
 package _mySolutions
 
+// Write two classes, Director and Film , with fields and methods as follows:
+//  • Director should contain:
+//    – a field firstName of type String
+//    – a field lastName of type String
+//    – a field yearOfBirth of type Int
+//    – a method called name that accepts no parameters and returns the full name
 class Director(
     val firstName: String,
     val lastName: String,
     val yearOfBirth: Int
 ) {
+  def name = s"$firstName $lastName $yearOfBirth"
 
-  def name: String = s"$firstName $lastName"
-
-  def copy(
-      firstName: String = this.firstName,
-      lastName: String = this.lastName,
-      yearOfBirth: Int = this.yearOfBirth
-  ) =
+  def copy(firstName: String, lastName: String, yearOfBirth: Int): Director =
     new Director(firstName, lastName, yearOfBirth)
 }
 
+// • Film should contain:
+//   - a field name of type String
+//   – a field yearOfRelease of type Int
+//   – a field imdbRating of type Double
+//   – a field director of type Director
+//   – a method directorsAge that returns the age of the director at the time of release
+//   - a method isDirectedBy that accepts a Director as a parameter and returns a Boolean
 class Film(
     val name: String,
     val yearOfRelease: Int,
     val imdbRating: Double,
     val director: Director
 ) {
-  def directorsAge: Int = yearOfRelease - director.yearOfBirth
+  def directorsAge(director: Director, film: Film): Int =
+    film.yearOfRelease - director.yearOfBirth
 
   def isDirectedBy(director: Director): Boolean =
     this.director == director
+//      if (director == this.director)
+//        true
+//      else
+//        false
 
-//  Implement a method of Film called copy . This method should accept the
-//  same parameters as the constructor and create a new copy of the film. Give
-//  each parameter a default value so you can copy a film changing any subset of
-//  its values.
   def copy(
-          name: String = this.name,
-          yearOfRelease: Int = this.yearOfRelease,
-          imdbRating: Double = this.imdbRating,
-          director: Director = this.director): Film =
-  new Film(name, yearOfRelease, imdbRating, director)
+      name: String = "Natale",
+      yearOfRelease: Int = 1990,
+      imdbRating: Double = 10,
+      director: Director = someBody
+  ): Film =
+    new Film(name, yearOfRelease, imdbRating, director)
+
+  val eastwood = new Director("Clint", "Eastwood", 1930)
+  val mcTiernan = new Director("John", "McTiernan", 1951)
+  val nolan = new Director("Christopher", "Nolan", 1970)
+  val someBody = new Director("Just", "SomeBody", 1990)
+
+  val inception = new Film("Inception", 2010, 8.8, nolan)
+  val granTorino = new Film("Gran Torino", 2008, 8.2, eastwood)
+  val dieHard = new Film("Die Hard", 1988, 8.3, mcTiernan)
+  val something = new Film("something", 1994, 10, someBody)
+
+
 }
