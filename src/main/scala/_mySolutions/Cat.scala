3c3f00a62010c52a3cfaf28775ea7961f7f689ea
Simona Debilio <simona.debilio@gmail.com>
2020-09-28 15:03:20 +0100

    Merge Cat with CaseCats and Counter with CaseCounter

--- src/main/scala/_mySolutions/Cat.scala
+++ src/main/scala/_mySolutions/Cat.scala
@@ -1,16 +1,24 @@
 package _mySolutions
 
 // Define a class Cat and then create an object for each cat
-class Cat(val colour: String = "colour", val food: String = "food") {
-  val oswald = new Cat("Black", "Milk")
-  val henderson = new Cat("Ginger", "Chips")
-  val quentin = new Cat("Tabby and white", "Curry")
+case class Cat(colour: String = "colour", food: String = "food") {
+  val oswald = Cat("Black", "Milk")
+  val henderson = Cat("Ginger", "Chips")
+  val quentin = Cat("Tabby and white", "Curry")
 }
 
-// Define an object ChipShop with a method willServe . This method should
+// Define an object ChipShop with a method willServe. This method should
 //  accept a Cat and return true if the cat’s favourite food is chips, and false
 //  otherwise.
 object ChipShop {
   def willServe(c: Cat): Boolean =
     if (c.food == "Chips") true else false
+}
+
+object ChipShops2 {
+  def willServe(cat: Cat): Boolean =
+    cat match {
+      case Cat(_, "Chips") => true
+      case Cat(_, _)    => false
+    }
 }
--- src/main/scala/_mySolutions/Counter.scala
+++ src/main/scala/_mySolutions/Counter.scala
@@ -4,7 +4,15 @@ package _mySolutions
 //  The methods inc and dec should increment and decrement the counter respectively returning a new Counter .
 class Counter(val count: Int) {
   def inc = new Counter(count + 1)
+
   def dec = new Counter(count - 1)
+
+// Reimplement Counter as a case class, using copy where appropriate.
+//  Additionally initialise count to a default value of 0.
+//case class CaseCounter(count: Int = 0) {
+//  def dec: CaseCounter = copy(count = count - 1)
+//  def inc: CaseCounter = copy(count = count + 1)
+//}
 
 //  Augment the Counter from the previous exercise to allow the user
 //    can optionally pass an Int parameter to inc and dec.
@@ -15,7 +23,6 @@ class Counter(val count: Int) {
 // Extend Counter to add a method called adjust . This method should accept
 //  an Adder and return a new Counter with the result of applying the Adder to the count.
   def adjust(a: Adder): Counter = new Counter(a.add(count))
-
 }
 
 class Adder(amount: Int) {
